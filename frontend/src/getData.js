e000ee3a130176a31407264a4e062bfe8b322810
jadelynx912 <karicordes@arizona.edu>
2024-12-02 09:34:48 -0800

    dev: commit timeline

--- frontend/src/getData.js
+++ frontend/src/getData.js
@@ -1,8 +1,11 @@
 import React from "react";
-import myData from "./data/IR323_3138.json";
+//import myData from "./data/IR323_3138.json";
 
 
-export default function getData(nodes_array){
+export default function getData(myData, nodes_array){
+    if (myData == null){
+        return null;
+    }
     let microservices = myData["microservices"];
     let nodes = [];
     let methods = {};
@@ -35,7 +38,7 @@ export default function getData(nodes_array){
                 let url = method["url"];
                 let http = method["httpMethod"];
                 // Check if this method has a default annotation, then also add that url
-                if ("default" in method["annotations"][0]["attributes"]){
+                if (method["annotations"].length > 0 && "default" in method["annotations"][0]["attributes"]){
                     let temp_url = method["annotations"][0]["attributes"]["default"];
                     methods[temp_url] = {
                         "microservice" : nodeName, 
@@ -55,15 +58,12 @@ export default function getData(nodes_array){
                     "methodName": methodName,
                     "httpMethod": http,
                 }
-               
-
+            }
         }
     }
-   
-}
 
-let connections = [];
-let links = [];
+    let connections = [];
+    let links = [];
     // array can be controller or service
     function iterateThrough(array){
         for (let i=0; i<array.length; i++){
@@ -84,13 +84,13 @@ let links = [];
                 
                 let http = methodCall["httpMethod"];
                 let className;
-            if (arr["implementedTypes"].length == 1){ 
-                className = arr["implementedTypes"][0];
+                if (arr["implementedTypes"].length == 1){ 
+                    className = arr["implementedTypes"][0];
 
-            }
-            else{
-                className = arr["name"];
-            }
+                }
+                else{
+                    className = arr["name"];
+                }
                 let calledFrom = methodCall["calledFrom"];
                 let destination = methods[url]["microservice"];
                 let source = methodCall["microserviceName"];
@@ -106,7 +106,7 @@ let links = [];
                                 "source": source,
                                 "target": destination,
                                 "requests": [
-                                  {
+                                {
                                     "destinationUrl": url,
                                     "sourceMethod": calledFrom,
                                     "endpointFunction": methodCall["name"],
@@ -115,10 +115,10 @@ let links = [];
                                     "type": http,
                                     "argument": parameters,
                                     "msReturn": methods[url]["returnType"],
-                                  }
+                                }
                                 ],
                                 "name": name
-                              },
+                            },
                         )
     
                     }
@@ -138,30 +138,26 @@ let links = [];
                                 "type": http,
                                 "argument": parameters,
                                 "msReturn": methods[url]["returnType"],
-                              }
+                            }
     
                         )
-    
                     }
                 } 
+            }
         }
     }
-    
 
+    for (let i=0; i<microservices.length;i++){
+        let microservice = microservices[i];
+        let nodeName = microservice["name"];
+        if (!(nodes_array == undefined) && !(nodes_array.includes(nodeName))){
+            continue;
+        }
+        let controllers = microservice["controllers"];
+        let services = microservice["services"];
+        iterateThrough(services);
+        iterateThrough(controllers);
     }
-
-for (let i=0; i<microservices.length;i++){
-    let microservice = microservices[i];
-    let nodeName = microservice["name"];
-    if (!(nodes_array == undefined) && !(nodes_array.includes(nodeName))){
-        continue;
-    }
-    let controllers = microservice["controllers"];
-    let services = microservice["services"];
-    iterateThrough(services);
-    iterateThrough(controllers);
-    
-}
 
     return {
         "graphName": "msgraph",
@@ -169,6 +165,4 @@ for (let i=0; i<microservices.length;i++){
         "links": links, 
         "gitCommitId": "0"
     };
-
-
 }
